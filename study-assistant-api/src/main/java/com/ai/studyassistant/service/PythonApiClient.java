6ea907398c9ab01633c9e335a447bca4ab5fae2e
junaeid <junaeidahmed13@gmail.com>
2026-08-02 14:47:25 +0200

    Refactor duplicate services API communication logic closes #53

---
+++ study-assistant-api/src/main/java/com/ai/studyassistant/service/PythonApiClient.java
@@ -0,0 +1,66 @@
+package com.ai.studyassistant.service;
+
+import org.springframework.beans.factory.annotation.ParameterResolutionDelegate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class PythonApiClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${python.api.url}")
+    private String pythonApiUrl;
+
+    public PythonApiClient(
+            RestTemplate restTemplate
+    ) {
+        this.restTemplate = restTemplate;
+    }
+
+    public <T, R> List<R> post(
+            String endpoint,
+            T requestBody,
+            ParameterizedTypeReference<List<R>> responseType
+    ) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<T> request = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<List<R>> response =
+                restTemplate.exchange(
+                        pythonApiUrl + endpoint,
+                        HttpMethod.POST,
+                        request,
+                        responseType
+                );
+
+        if (response.getBody() == null || response.getBody().isEmpty()) {
+            throw new RuntimeException(
+                    "Python Api returned empty response"
+            );
+        }
+        return response.getBody();
+    }
+
+    public String uploadFile(
+            String endpoint,
+            HttpEntity<?> request
+    ) {
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(
+                        pythonApiUrl + endpoint,
+                        request,
+                        String.class
+                );
+
+        return response.getBody();
+    }
+}
