6d1e0a7ee63135f3d8018d9c916bd2f57d763c12
junaeid <junaeidahmed13@gmail.com>
2026-08-30 11:49:03 +0200

    add document chunk entity

---
+++ study-assistant-api/src/main/java/com/ai/studyassistant/entity/DocumentChunk.java
@@ -0,0 +1,30 @@
+package com.ai.studyassistant.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.swing.event.DocumentEvent;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "document_chunks")
+public class DocumentChunk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    private Integer chunkIndex;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "document_id", nullable = false)
+    private Document document;
+}
