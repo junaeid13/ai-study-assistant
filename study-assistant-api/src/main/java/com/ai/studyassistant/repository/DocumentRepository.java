22b4439cc137ca009fecf37790aea13fb43cd356
junaeid <junaeidahmed13@gmail.com>
2026-09-16 19:58:25 +0200

    add method find by id and username in document repo

--- study-assistant-api/src/main/java/com/ai/studyassistant/repository/DocumentRepository.java
+++ study-assistant-api/src/main/java/com/ai/studyassistant/repository/DocumentRepository.java
@@ -3,5 +3,11 @@ package com.ai.studyassistant.repository;
 import com.ai.studyassistant.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+List<Document> findByUserUsername(String username);
+Optional<Document> findByIdAndUserUsername(Long id, String username);
 }
