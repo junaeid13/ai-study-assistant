5392735c8445325e6902c7f10553f420e7856d61
junaeid <junaeidahmed13@gmail.com>
2026-09-25 17:00:11 +0200

    refactor handledocument not found methond and added new exception

--- study-assistant-api/src/main/java/com/ai/studyassistant/exception/GlobalExceptionHandler.java
+++ study-assistant-api/src/main/java/com/ai/studyassistant/exception/GlobalExceptionHandler.java
@@ -6,17 +6,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(DocumentNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleDocumentNotFound(
             DocumentNotFoundException ex
     ) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(
-                        HttpStatus.NOT_FOUND.value(),
-                        ex.getMessage()
-                ));
+        return buildErrorResponse(
+                HttpStatus.NOT_FOUND,
+                ex.getMessage()
+        );
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(
+            Exception ex
+    ) {
+        return buildErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "an unexpected error occurred."
+        );
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(
+            HttpStatus status,
+            String message
+    ) {
+        return ResponseEntity.status(status)
+                .body(new ErrorResponse(status.value(), message));
     }
 }
