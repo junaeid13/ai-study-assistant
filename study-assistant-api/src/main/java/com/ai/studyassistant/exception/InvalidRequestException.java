25f362b4167788d3a19ecc8f46dc1c9ec00acb56
junaeid <junaeidahmed13@gmail.com>
2026-09-25 16:05:26 +0200

    add invalid request exception

---
+++ study-assistant-api/src/main/java/com/ai/studyassistant/exception/InvalidRequestException.java
@@ -0,0 +1,7 @@
+package com.ai.studyassistant.exception;
+
+public class InvalidRequestException extends RuntimeException {
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+}
